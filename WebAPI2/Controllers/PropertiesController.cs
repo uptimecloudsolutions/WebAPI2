ccb7f6614b8f3001d0c2e04fbc1433792b270a29
Leon Storey <lzs@townsville.qld.gov.au>
2019-08-26 15:45:51 +1000

    Add project files.

---
+++ WebAPI2/Controllers/PropertiesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.UI.WebControls;
+using WebAPI2;
+
+namespace WebAPI2.Controllers
+{
+    public class PropertiesController : ApiController
+    {
+        private InterfacesEXTEntities db = new InterfacesEXTEntities();
+
+
+        // GET: api/Properties
+        //<summary>Returns all properties loaded from PnR Nightly</summary>
+        //<returns>JSON array containing all properties</returns>
+        //<param name="locality">Limit by given Locality</param>
+        //public IQueryable<Property> GetProperties()//(string? locality)
+        public IQueryable<Property> GetProperties()
+        {
+            //InterfacesEXTEntities db = new InterfacesEXTEntities();
+            //return Request.CreateResponse(db.Properties.Where(e => e.locality = locality));
+            return db.Properties.AsQueryable();
+
+           
+
+        }
+
+        // GET: api/Properties/5
+        [ResponseType(typeof(Property))]
+        public IHttpActionResult GetProperty(int id)
+        {
+            Property property = db.Properties.Find(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(property);
+        }
+
+        // PUT: api/Properties/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProperty(int id, Property property)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != property.property_number)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(property).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PropertyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Properties
+        [ResponseType(typeof(Property))]
+        public IHttpActionResult PostProperty(Property property)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Properties.Add(property);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PropertyExists(property.property_number))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = property.property_number }, property);
+        }
+
+        // DELETE: api/Properties/5
+        [ResponseType(typeof(Property))]
+        public IHttpActionResult DeleteProperty(int id)
+        {
+            Property property = db.Properties.Find(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            db.Properties.Remove(property);
+            db.SaveChanges();
+
+            return Ok(property);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PropertyExists(int id)
+        {
+            return db.Properties.Count(e => e.property_number == id) > 0;
+        }
+    }
+}
